4c2957d7825cfb5d6fca4c5f7e9dec0f24d318f6
jbh0630 <jayjo0630@gmail.com>
2021-07-28 19:11:42 +0000

    fix bugs and screenshots

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by_email(params[:email])
-    if user = User.authenticate_with_credentials(params[:email], params[:password])
+
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to '/'
     else
@@ -15,7 +16,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to '/login'
+    redirect_to :root
   end
 
 end
